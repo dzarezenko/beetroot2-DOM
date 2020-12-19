be3dd4420f9d5d314abe9ea1b9d7e9c663082c0d
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2020-12-19 11:51:00 +0200

    Children and Parent

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log(document.body.childNodes);
+
+let form = document.body.childNodes[1];
+console.log(form.childNodes);
+
+let input = form.childNodes[1];
+console.log(input.childNodes);
+
+console.log(input.parentNode);
